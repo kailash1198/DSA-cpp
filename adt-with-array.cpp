76168a7d223e7a08efc1ee1c3cbde5d7a33e5875
kailash kumar <kailash414243@gmail.com>
2024-02-13 20:44:16 +0530

    Array element deletion practice

--- adt-with-array.cpp
+++ adt-with-array.cpp
@@ -2,27 +2,44 @@
 #include <string>
 using namespace std;
 
-struct array
+// struct array
+// {
+//     int *A;
+//     int size;
+//     int length;
+// };
+
+// custum array data types
+struct myArray
 {
-    int *A;
-    int size;
-    int length;
+    int totalSize;
+    int usedSize;
+    int *ptr;
 };
+
+void createArray(struct myArray *a, int tSize, int uSize)
+{
+    (*a).totalSize = tSize;
+    (*a).usedSize = uSize;
+    (*a).ptr = (int *)malloc(tSize * sizeof(int));
+}
 
 int main(void)
 {
-    struct array arr;
-    cout << "Enter array size : ";
-    cin >> arr.size;
+    struct myArray marks;
+    createArray(&marks, 100, 20);
+    // struct array arr;
+    // cout << "Enter array size : ";
+    // cin >> arr.size;
 
-    arr.A = (int *)malloc(arr.size * sizeof(int));
-    arr.length = 0;
+    // arr.A = (int *)malloc(arr.size * sizeof(int));
+    // arr.length = 0;
 
-    cout<<"Please enter number of elements here : ";
-    cin>>arr.length;
+    // cout<<"Please enter number of elements here : ";
+    // cin>>arr.length;
 
-    cout<<"Enter element one-by-one";
-    for(int i=0; i<arr.length; i++){
-        cin>>arr.A[i];
-    }
+    // cout<<"Enter element one-by-one";
+    // for(int i=0; i<arr.length; i++){
+    //     cin>>arr.A[i];
+    // }
 }
---
+++ array-deletion.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// struct myArray
+// {
+//     int size;
+//     int length;
+//     int *ptr;
+// };
+
+// struct myArray arrayCreation;
+
+// void arrayDelete()
+// {
+//     delete arrayCreation.ptr;
+// }
+
+
+
+
+
+int main(void)
+{
+
+    // int arrayOne[2] = {10, 20};
+    // cout << "Enter array size : ";
+    // cin >> arrayCreation.size;
+
+    // arrayCreation.ptr = new int(arrayCreation.size);
+    // cout << "Enter length of Array: ";
+    // cin >> arrayCreation.length;
+
+    // cout << "Array is now created" << endl
+    //      << endl;
+    // cout << "Enter elements of the new arrys : ";
+    // for (int i = 0; i < arrayCreation.length; i++)
+    // {
+    //     cin >> arrayCreation.ptr[i];
+    // }
+
+    // for (int j = 0; j < arrayCreation.length; j++)
+    // {
+    //     cout << "Your element is = " << arrayCreation.ptr[j] << endl;
+    // }
+
+    // arrayDelete();
+}
