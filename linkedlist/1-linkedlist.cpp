a530370aa10f979fdfa9bdc6afe945a0ae37bb52
kailash kumar <kailash414243@gmail.com>
2024-02-22 00:56:18 +0530

    Basic linked list practice

---
+++ linkedlist/1-linkedlist.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+// single linked list
+//  class node
+//  {
+//  public:
+//      int data;
+//      node *next;
+//  };
+
+// doubled linked list
+// class node
+// {
+// public:
+//     int data;
+//     node *next;
+//     node *prev;
+// };
+
+// circular linked list
+// class node
+// {
+// public:
+//     int data;
+//     node *next;
+// };
+
+int main(void)
+{
+}
