9149be76fd94b171663a38450064fb4af6cb74f8
kailash1198 <kailash414243@gmail.com>
2024-03-06 18:45:18 +0530

    array practice

--- array/practice/3-array-practice.cpp
+++ array/practice/3-array-practice.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
+using namespace std;
 
 //  Write a program to rotate the elements of an integer array to the left by 'n' positions.
+// example - {1},2,3,4,5,6,7 = 2,3,4,5,6,7,{1}
+
+void rotateLeft(int array[], int n, int pos){
+
+}
+
+int main(void)
+{
+    int arrayOne[7] = {1, 2, 3, 4, 5, 6, 7};
+    int pos = 2;
+    
+    for (int j = 0; j < 7; j++)
+    {
+        cout << arrayOne[j] << " ";
+    }
+    cout << endl;
+}
