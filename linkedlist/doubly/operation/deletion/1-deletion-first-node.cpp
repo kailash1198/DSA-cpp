ec4de8812b97c616320ad0b957e1f129c3285aff
kailash kumar <kailash414243@gmail.com>
2024-04-14 16:44:59 +0530

    first and last node deletion in doubly linked listd

---
+++ linkedlist/doubly/operation/deletion/1-deletion-first-node.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+class node
+{
+public:
+    int data;
+    node *next;
+    node *prev;
+    node(int value)
+    {
+        data = value;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+class doublyList
+{
+private:
+    node *head;
+    node *tail;
+
+public:
+    doublyList()
+    {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void addingNode(int dataValue)
+    {
+        node *newNode = new node(dataValue);
+        if (head == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void displayList()
+    {
+        node *current = head;
+        while (current != nullptr)
+        {
+            std::cout << current->data << std::endl;
+            current = current->next;
+        }
+    }
+
+    void firstNodeDeletion()
+    {
+        node *current = head;
+        current = current->next;
+        head = current;
+    }
+};
+
+int main()
+{
+    doublyList listOne;
+    listOne.addingNode(1);
+    listOne.addingNode(2);
+    listOne.addingNode(3);
+    listOne.addingNode(4);
+    listOne.addingNode(5);
+    listOne.addingNode(6);
+    listOne.addingNode(7);
+    listOne.addingNode(8);
+    listOne.firstNodeDeletion();
+    listOne.displayList();
+    return 0;
+}
